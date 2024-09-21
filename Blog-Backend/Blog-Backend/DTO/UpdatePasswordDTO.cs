68a323d46a52ae55866ba2fd45b996e30a0a5ddb
Pathum Lakshan Dissanayake <iamartseeker@gmail.com>
2024-09-21 16:26:14 +0530

    UpdatePasswordDTO done

---
+++ Blog-Backend/Blog-Backend/DTO/UpdatePasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog_Backend.DTO
+{
+    public class UpdatePasswordDTO
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
